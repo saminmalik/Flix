7ab318fbcdba0e131cb90bf2e62e3db23723f294
saminmalik <samin.malik@stonybrook.edu>
2021-02-24 23:41:43 -0500

    I have Added More features into my app Flix: -another tab to see what other superhero movies are playing -Also now you can tap on each movies from 'Now Playing' to see more description

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Samin Malik on 2/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
